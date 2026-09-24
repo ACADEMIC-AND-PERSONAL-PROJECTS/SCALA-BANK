d8e428ec6422f3540b1116a15122a2cfe03d73c5
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-09-24 15:52:10 +0000

    AccountService deposit method added

--- src/main/scala/services/AccountService.scala
+++ src/main/scala/services/AccountService.scala
@@ -1,9 +1,12 @@
 package me.khadimprojects
 package services
 
-import models.{Account, AccountType, Customer, Devise}
+import models.{Account, AccountType, Customer, Devise, Transaction}
 import storage.InMemoryStorage
 import models.AccountStatut.ACTIVE
+
+import models.Devise.USD
+import models.TransactionType.DEPOSIT
 
 import java.util.UUID
 import java.time.LocalDateTime
@@ -17,19 +20,45 @@ class AccountService:
               devise: Devise,
               customer: Customer): Either[String, (Account, InMemoryStorage)] = {
 
-      // Initiate account
-      val account = Account(
-        generateId(),
-        BigDecimal(0.0),
-        accountType,
-        ACTIVE,
-        LocalDateTime.now(),
-        devise,
-        customer
-      )
-      val newStorage = storage.addAccount(account)
-      Right((account, newStorage))
+    // Initiate account
+    val account = Account(
+      generateId(),
+      BigDecimal(0.0),
+      accountType,
+      ACTIVE,
+      LocalDateTime.now(),
+      devise,
+      customer
+    )
 
+    // Add an account into the in memory storage
+    val newStorage = storage.addAccount(account)
+
+    // Return the new account created and the storage
+    Right((account, newStorage))
+
+  }
+
+  // Deposit
+  def deposit(storage: InMemoryStorage,
+              accountId: String,
+              amount: BigDecimal): Either[String, (Transaction, InMemoryStorage)] = {
+    for {
+      // Find the account
+      account <- storage.findAccount(accountId).toRight("Account not found")
+
+      // Update account
+      updatedAccount = account.copy(balance = account.balance + amount)
+
+      // Retrieve the new storage
+      storage1 = storage.addAccount(updatedAccount)
+
+      // Create a transaction
+      deposit = Transaction(generateId(), accountId, amount, DEPOSIT, LocalDateTime.now(), USD)
+
+      // New storage with all values updated
+      storage2 = storage1.addTransaction(deposit)
+    } yield (deposit, storage2)
   }
 
   // Generate an id
